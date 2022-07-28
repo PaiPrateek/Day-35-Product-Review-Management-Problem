93fd7b9aab18ea7381fae105bf597f59beef2511
Prateek Pai <paiprateek123@gmail.com>
2022-07-28 22:58:56 +0530

    Retrieve all records who's Rating is Greater that 3 and Product ID is 1 or 4 or 9

--- ProductReviewManagement/ProductManagement.cs
+++ ProductReviewManagement/ProductManagement.cs
@@ -12,7 +12,7 @@ namespace ProductReviewManagement
         public static void Top3Records(List<ProductReview> Product)
         {
             var result = (from PM in Product orderby PM.Rating descending select PM).Take(3);
-            Console.WriteLine("Top 3 Records are :");
+            Console.WriteLine("\nTop 3 Records are :");
             foreach(ProductReview pm in result)
             {
                 Console.WriteLine("\nProduct ID: " + pm.ProductId +
@@ -21,6 +21,21 @@ namespace ProductReviewManagement
                                     "\nReview: "+ pm.Review +
                                     "\nIS Liked : "+pm.isLike);
             }
+        }
+        //Retrieve all records who's Rating is Greater that 3 and Product ID is 1 or 4 or 9
+        public static void RetriveAllRecordWhosRatingGreaterThan3(List<ProductReview> Product)
+        {
+            var result = (from PM in Product where (PM.ProductId > 3) && (PM.ProductId == 1 || PM.ProductId == 4 || PM.ProductId == 9) select PM).ToList();
+            Console.WriteLine("\nRetrieve all records who's Rating is Greater than 3 and Product ID is 14 or 4 or 9 :");
+            foreach (ProductReview pm in result)
+            {
+                Console.WriteLine("\nProduct ID: " + pm.ProductId +
+                                    "\nUser ID: " + pm.UserId +
+                                    "\nRating: " + pm.Rating +
+                                    "\nReview: " + pm.Review +
+                                    "\nIS Liked : " + pm.isLike);
+            }
+
         }
     }
 }
--- ProductReviewManagement/Program.cs
+++ ProductReviewManagement/Program.cs
@@ -18,7 +18,7 @@ namespace ProductReviewManagement
                 new ProductReview() { ProductId = 35, UserId = 111, Rating = 1, Review = "Bad", isLike = false },
                 new ProductReview() { ProductId = 80, UserId = 167, Rating = 5, Review = "Good", isLike = true },
                 new ProductReview() { ProductId = 16, UserId = 13, Rating = 4, Review = "Average", isLike = true },
-                new ProductReview() { ProductId = 85, UserId = 15, Rating = 4, Review = "Average", isLike = true },
+                new ProductReview() { ProductId = 1, UserId = 15, Rating = 4, Review = "Average", isLike = true },
                 new ProductReview() { ProductId = 10, UserId = 19, Rating = 1, Review = "Bad", isLike = false },
                 new ProductReview() { ProductId = 96, UserId = 12, Rating = 2, Review = "Bad", isLike = false },
                 new ProductReview() { ProductId = 50, UserId = 16, Rating = 3, Review = "Average", isLike = true },
@@ -26,13 +26,13 @@ namespace ProductReviewManagement
                 new ProductReview() { ProductId = 100, UserId = 178, Rating = 1, Review = "Bad", isLike = false },
                 new ProductReview() { ProductId = 30, UserId = 123, Rating = 3, Review = "Average", isLike = true },
                 new ProductReview() { ProductId = 90, UserId = 18, Rating = 4, Review = "Average", isLike = true },
-                new ProductReview() { ProductId = 20, UserId = 11, Rating = 5, Review = "Good", isLike = true },
+                new ProductReview() { ProductId = 4, UserId = 11, Rating = 5, Review = "Good", isLike = true },
                 new ProductReview() { ProductId = 70, UserId = 16, Rating = 5, Review = "Good", isLike = true },
                 new ProductReview() { ProductId = 10, UserId = 19, Rating = 5, Review = "Good", isLike = true },
                 new ProductReview() { ProductId = 90, UserId = 12, Rating = 2, Review = "Bad", isLike = false },
                 new ProductReview() { ProductId = 40, UserId = 15, Rating = 1, Review = "Bad", isLike = false },
                 new ProductReview() { ProductId = 60, UserId = 11, Rating = 4, Review = "Average", isLike = true },
-                new ProductReview() { ProductId = 20, UserId = 15, Rating = 1, Review = "Bad", isLike = false },
+                new ProductReview() { ProductId = 9, UserId = 15, Rating = 1, Review = "Bad", isLike = false },
                 new ProductReview() { ProductId = 80, UserId = 19, Rating = 5, Review = "Good", isLike = true },
                 new ProductReview() { ProductId = 30, UserId = 19, Rating = 5, Review = "Good", isLike = true },
                 new ProductReview() { ProductId = 50, UserId = 11, Rating = 2, Review = "Good", isLike = true },
@@ -40,9 +40,11 @@ namespace ProductReviewManagement
                 new ProductReview() { ProductId = 90, UserId = 19, Rating = 1, Review = "Good", isLike = true }
             };
 
-            
+            //Top 3 Records of High Rating
             ProductManagement.Top3Records(Product);
 
+            //Retrieve all records who's Rating is Greater that 3 and Product ID is 1 or 4 or 9
+            ProductManagement.RetriveAllRecordWhosRatingGreaterThan3(Product);
 
         }
 
