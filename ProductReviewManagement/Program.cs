8377029c6cb201d39811c5e9481a48de00e34931
Prateek Pai <paiprateek123@gmail.com>
2022-07-28 23:10:10 +0530

    Retrieve count of review present for each Product ID

--- ProductReviewManagement/Program.cs
+++ ProductReviewManagement/Program.cs
@@ -46,6 +46,9 @@ namespace ProductReviewManagement
             //Retrieve all records who's Rating is Greater that 3 and Product ID is 1 or 4 or 9
             ProductManagement.RetriveAllRecordWhosRatingGreaterThan3(Product);
 
+            //Retrieve count of review present for each Product ID
+            ProductManagement.RetrieveCountofReviewForEachProiductId(Product);
+
         }
 
     }
